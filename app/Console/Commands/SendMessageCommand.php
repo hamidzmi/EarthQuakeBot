e571a76b54285de1915144f01cda5db483e8088a
Hamid Zamani <hamidzamani.ce@gmail.com>
2020-05-15 01:54:45 +0430

    Get data from api

--- app/Console/Commands/SendMessageCommand.php
+++ app/Console/Commands/SendMessageCommand.php
@@ -35,13 +35,26 @@ class SendMessageCommand extends Command
         if ($response->status() != 200) {
             Log::critical("The response HTTP code of get data API is: " . $response->status());
         }
-
-        Log::log($response->body());
-//        $xml = simplexml_load_string($response->body());
-//        $json = json_encode($xml);
-//        $array = json_decode($json,TRUE);
-        $message = $response->body();
-        $response = Http::get("https://api.telegram.org/bot1138407370:AAGcehBntpDFAD8fOsRiOf-iLOV3oV0ovJI/sendMessage?chat_id=@IranianEarthquakes&text=" . $message);
-        Log::info("api response code: " . $response->status());
+        $xml = simplexml_load_string($response->body());
+        $json = json_encode($xml);
+        $events = json_decode($json,TRUE);
+        foreach ($events as $i => $event) {
+            if ($i == 0) {
+                continue;
+            }
+            if (i > 10) {
+                break;
+		    }
+            $message = sprintf(
+                "Region:%s\nDepth:%s\nTime:%s\nLocation:https://www.google.com/maps/search/?api=1&query=%s,%s\n", [
+                    $event["reg1"],
+                    $event["dep"],
+                    $event["date"],
+                    $event["lat"],
+                    $event["long"],
+                ]);
+            $response = Http::get("https://api.telegram.org/bot1138407370:AAGcehBntpDFAD8fOsRiOf-iLOV3oV0ovJI/sendMessage?chat_id=@IranianEarthquakes&text=" . $message);
+            Log::info("api response code: " . $response->status());
+        }
     }
 }
