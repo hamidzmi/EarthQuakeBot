4253c08a0221a5d752f1432da021c6237ba08030
Hamid Zamani <hamidzamani.ce@gmail.com>
2020-05-15 13:09:32 +0430

    Fixed bug

--- app/Console/Commands/SendMessageCommand.php
+++ app/Console/Commands/SendMessageCommand.php
@@ -32,32 +32,27 @@ class SendMessageCommand extends Command
     public function handle()
     {
         $response = Http::get("http://irsc.ut.ac.ir/events_list.xml");
+
         $xml = simplexml_load_string($response->body());
         $json = json_encode($xml);
         $events = json_decode($json,TRUE);
-//        dd($events['item'][1]);
-        $lastEvent = $events['item'][1];
-        $message = "Region: " . $lastEvent['reg1'] . "\n" .
-            "Depth: " . $lastEvent['dep'] . "\n" .
-            "Time: " . $lastEvent['date'] . "\n" .
-            "Location: https://www.google.com/maps/search/?api=1&query=".$lastEvent['lat'].",".$lastEvent['long']."\n";
-//        Http::get("https://api.telegram.org/bot1138407370:AAGcehBntpDFAD8fOsRiOf-iLOV3oV0ovJI/sendMessage?chat_id=@IranianEarthquakes&text=" . $message);
-//        foreach ($events as $i => $event) {
-//            if ($i == 0) {
-//                continue;
-//            }
-//            if (i > 1) {
-//                break;
-//		    }
-//            $message = sprintf(
-//                "Region:%s\nDepth:%s\nTime:%s\nLocation:https://www.google.com/maps/search/?api=1&query=%s,%s\n", [
-//                    $event["reg1"],
-//                    $event["dep"],
-//                    $event["date"],
-//                    $event["lat"],
-//                    $event["long"],
-//                ]);
-//            Http::get("https://api.telegram.org/bot1138407370:AAGcehBntpDFAD8fOsRiOf-iLOV3oV0ovJI/sendMessage?chat_id=@IranianEarthquakes&text=" . $message);
-//        }
+
+        foreach ($events['item'] as $i => $event) {
+            if ($i == 0) {
+                continue;
+            }
+            if ($i > 1) {
+                break;
+		    }
+            $message = sprintf(
+                "Region:%s\nDepth:%s\nTime:%s\nLocation:https://www.google.com/maps/search/?api=1&query=%s,%s\n",
+                    $event["reg1"],
+                    $event["dep"],
+                    $event["date"],
+                    explode(" ", $event["lat"])[0],
+                    explode(" ", $event["long"])[0]
+                );
+            Http::get("https://api.telegram.org/bot1138407370:AAGcehBntpDFAD8fOsRiOf-iLOV3oV0ovJI/sendMessage?chat_id=@IranianEarthquakes&text=" . $message);
+        }
     }
 }
